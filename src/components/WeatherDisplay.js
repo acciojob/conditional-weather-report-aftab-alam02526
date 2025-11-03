1be0fbf1acb94c0d316be68f45a327be61e52be6
aftab-alam02526 <aftab0447@gmail.com>
2025-11-03 12:02:20 +0530

    Create WeatherDisplay.js

---
+++ src/components/WeatherDisplay.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function WeatherDisplay({ weather }) {
+  const { temperature, conditions } = weather;
+
+  const tempStyle = {
+  color : temperature > 20 ? "red" : "blue",
+    fontWeight: "bold",
+  };
+  return (
+    <div>
+      <p>
+        Temperature: <span style={tempStyle}>{temperature}°C</span>
+      </p>
+      <p>Conditions: {conditions}</p>
+    </div>
+  );
+}
+
+export default WeatherDisplay;
